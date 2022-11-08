40d5aa029cc2c6f1ba04e711fc3d7336c0782186
Kayane Costa <730kayanecosta@gmail.com>
2022-11-08 17:02:45 -0300

    Created spring project

---
+++ src/main/java/br/com/eemauricio/BackControleNotasApplication.java
@@ -0,0 +1,13 @@
+package br.com.eemauricio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackControleNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackControleNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/eemauricio/BackControleNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.eemauricio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackControleNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
